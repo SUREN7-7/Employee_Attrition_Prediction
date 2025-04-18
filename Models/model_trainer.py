f6517d32dc0d44b3247b9f636ceca817178ea6b3
Suren <shyamsaratsuren77@gmail.com>
2025-04-18 22:16:12 +0530

    modeltraining

---
+++ Models/model_trainer.py
@@ -0,0 +1,42 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+import pickle
+
+def train_model(df):
+    X = df.drop('Attrition', axis=1)
+    y = df['Attrition']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    models = {
+        "Logistic Regression": LogisticRegression(max_iter=1000),
+        "Decision Tree": DecisionTreeClassifier(),
+        "Random Forest": RandomForestClassifier(),
+        "Support Vector Machine": SVC(),
+        "Naive Bayes": GaussianNB()
+    }
+
+    best_model_name = None
+    best_model = None
+    best_accuracy = 0
+
+    for name, model in models.items():
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f"{name} Accuracy: {accuracy:.4f}")
+
+        if accuracy > best_accuracy:
+            best_accuracy = accuracy
+            best_model_name = name
+            best_model = model
+
+    print(f"\nBest Model: {best_model_name} with Accuracy: {best_accuracy:.4f}")
+    
+    with open('Models/best_model.pkl', 'wb') as f:
+        pickle.dump(best_model, f)
+
+    return best_model_name, best_model, best_accuracy
--- testing.py
+++ testing.py
@@ -1,20 +1,17 @@
 from Models.preprocessing import load_data, encode_categorical, scale_features, drop_irrelevant_columns
+from Models.model_trainer import train_model
 
 def test_preprocessing():
+    
     file_path = "C:\\SUREN_NEW\\Employee_Attrition_Prediction\\Data\\HR_Employee_Attrition.csv"
     df = load_data(file_path)
     df = drop_irrelevant_columns(df)
     df = encode_categorical(df)
     df = scale_features(df)
-    print("\nPreprocessing completed successfully!\n")
-    print("\nShape of processed data:", df.shape)
-    print("\nColumns:", df.columns.tolist())
-    print("\nSample data:\n", df.head())
-    
-    # # Optional: Check if target column exists
-    if 'Attrition' in df.columns:
-        print("\nTarget distribution:\n", df['Attrition'].value_counts())
+        
+    train_model(df)
 
-# Run the test
+
+
 if __name__ == "__main__":
     test_preprocessing()
